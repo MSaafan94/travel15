452711cec0f17fd47d62fa0bbddbb8941a921c81
m.saafan <m.saafan@mrexcel-app.com>
2023-07-27 15:07:15 +0300

    fix

--- sales_extra_fields/wizard/transfer_salesperson.py
+++ sales_extra_fields/wizard/transfer_salesperson.py
@@ -3,29 +3,16 @@ from odoo.exceptions import UserError
 
 
 class TransferSalesPerson(models.TransientModel):
-    """This model is used to transfer a salesperson from one lead to another."""
-
     _name = 'transfer.salesperson'
-    _description = 'Transfer SalesPerson'
+    _description = 'Transfer salesPerson'
 
-    employee_id = fields.Many2one('res.users', "User", required=True)
+    employee_id = fields.Many2one('hr.employee', "Employee", required=1)
 
     def transfer_sales_person(self):
-        """
-        This method transfers the salesperson associated with the active model's leads
-        to the salesperson associated with the selected user.
-        """
-        active_model_name = self.env.context.get('active_model')
-        active_ids = self.env.context.get('active_ids')
-
-        # Get all leads associated with the active model
-        leads = self.env[active_model_name].browse(active_ids)
-
-        # Transfer the salesperson for each lead
-        for lead in leads:
-            if not self.employee_id:
-                # Raise an error if the user doesn't exist
-                raise UserError(f"The user {self.employee_id.name} does not exist.")
+        model = self.env.context.get('active_model')
+        crm_ids = self.env[model].browse(self.env.context.get('active_ids'))
+        for lead in crm_ids:
+            if not self.employee_id.user_id:
+                raise UserError(_("You Should Set User for Employee %s") % (self.employee_id.name))
             else:
-                # Transfer the salesperson
-                lead.user_id = self.employee_id.id
+                lead.user_id = self.employee_id.user_id.id
